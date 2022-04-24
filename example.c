1a1e084bebcf0882aaa1b90d9e42a19dd718e46c
Reo Hakuta <reonaldhkt+webs@gmail.com>
2022-04-25 00:38:32 +0900

    Implemented findMatches()

--- example.c
+++ example.c
@@ -1,19 +1,27 @@
-#include <stdio.h>
 #include <regex.h>
+#include <stdio.h>
 
-int main(void){
-  const char regex[] = "^(070|080|090)-[0-9]{4}-[0-9]{4}$";
-  regex_t preg; char str[20]; regmatch_t pm[1];
+#define GROUP_NUMBER 5
 
-  if(regcomp(&preg, regex, REG_EXTENDED|REG_NEWLINE) != 0){
+int main(void) {
+  // const char regex[] = "(070|080|090)-[0-9]{4}-[0-9]{4}";
+  const char regex[] = "(0[7,8,9]0)-([0-9]{3,4})-([0-9]{4,5})";
+  regex_t preg;
+  char str[40] = "070-1323-4500 090-9684-8271";
+  regmatch_t pm[GROUP_NUMBER];
+
+  if (regcomp(&preg, regex, REG_EXTENDED | REG_NEWLINE) != 0) {
     printf("Regex is not available.\n");
     return 1;
   }
 
-  printf("Enter a phone number: "); scanf("%s", str);
-  if(regexec(&preg, str, 1, pm, 0) == 0){
+  // printf("Enter a phone number: "); scanf("%s", str);
+  if (regexec(&preg, &str[13], GROUP_NUMBER, pm, 0) == 0) {
     printf("It is Japanese one.\n");
-  }else{
+    for (int i = 0; i < GROUP_NUMBER; i++) {
+      printf("%d -> %d\n", pm[i].rm_so, pm[i].rm_eo);
+    }
+  } else {
     printf("It belongs to a foreign country.\n");
   }
 
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 
 #define ERROR_FILEHANDLE -1
 #define ERROR_REGEX -2
@@ -11,88 +11,140 @@
 #define LINE_LENGTH 128
 #define PHONENUMBER_COUNT 8
 
-int readTextFile(char *dest, char *fileName){
+int readTextFile(char *dest, char *fileName)
+{
   FILE *filePointer;
   filePointer = fopen(fileName, "r");
-  if(filePointer == NULL) return ERROR_FILEHANDLE;
+  if (filePointer == NULL)
+    return ERROR_FILEHANDLE;
 
   // destに収まることができる最小の行数分だけくりかえす
-  for(int i = 0; i < (STR_LENGTH / LINE_LENGTH); i++){
+  for (int i = 0; i < (STR_LENGTH / LINE_LENGTH); i++)
+  {
     // printf("%d of %d -th reading**\n", i, (STRLEN / CHARSPERLINE));
     char currentLine[LINE_LENGTH];
-    if(fgets(currentLine, LINE_LENGTH, filePointer) == NULL){
+    if (fgets(currentLine, LINE_LENGTH, filePointer) == NULL)
+    {
       break;
-    }else{
+    }
+    else
+    {
       strcat(dest, currentLine);
     }
   }
 
-  if(fclose(filePointer) == EOF){
+  if (fclose(filePointer) == EOF)
+  {
     return ERROR_FILEHANDLE;
-  }else{
+  }
+  else
+  {
     return 0;
   }
 }
 
+int findMatches(char *regex, char *source, char (*dest)[PHONENUMBER_LENGTH])
+{
+  // REFER:
+  // https://www.boost.org/doc/libs/1_60_0/libs/regex/doc/html/boost_regex/syntax/basic_extended.html
 
+  static const unsigned int maxMatches = PHONENUMBER_COUNT;
+  static const unsigned int maxSubGroups = 3;
 
-int findMatches(char *regex, char *source, char (*dest)[PHONENUMBER_LENGTH], int maxMatches){
-    // REFER: https://www.boost.org/doc/libs/1_60_0/libs/regex/doc/html/boost_regex/syntax/basic_extended.html
-
-  // const char regex[] = "0[7,8,9]0-\\d{3,4}-\\d{4,5}";
-  // const char regex[] = "0[7,8,9]0-[[:digit:]]{3,4}-[[:digit:]]{4,5}";
-  // const char regex[] = "0[7,8,9]0-[0-9]{3,4}-[0-9]{4,5}";
-  regex_t preg; 
-  regmatch_t matchResults[maxMatches];
+  regex_t preg;
+  regmatch_t resultGroups[maxMatches];
   int matchCount = 0;
 
   printf("maxMatches: %d\n", maxMatches);
-  if(regcomp(&preg, regex, REG_EXTENDED|REG_NEWLINE) != 0){
+  if (regcomp(&preg, regex, REG_EXTENDED | REG_NEWLINE) != 0)
+  {
     return ERROR_REGEX;
   }
 
-  // 戻り値は０で一致、それいがいで不一致
-  if(regexec(&preg, source, maxMatches, matchResults, 0) == 0){
-    for(int i = 0; i < maxMatches; i++){
-      printf("start: %d, end: %d\n", matchResults[i].rm_so, matchResults[i].rm_eo);
-      if(matchResults[i].rm_so != -1 && matchResults[i].rm_eo != -1){
-        matchCount++;
-        int targetLength = matchResults[i].rm_eo - matchResults[i].rm_so;
-        strncpy(dest[i], &source[matchResults[i].rm_so], matchResults[i].rm_eo - matchResults[i].rm_so);
+  // unsigned int offset = 0;
+  char *cursor = source, *nextCursor;
+  for (unsigned int match = 0; match < maxMatches; match++)
+  {
+    // 正規表現に該当する部分の数だけ、くりかえす
+    // 文字列sourceのoffset以降の部分文字列に正規表現を実行
+    if (regexec(&preg, cursor, (size_t)maxMatches, resultGroups, 0) != 0)
+    {
+      break; // 当該部分がみつからなかったら、終了
+    }
+    else
+    {
+      // 当該部分が見つかったら、
+      matchCount++;
+      for (unsigned int group = 0; group < 1 + maxSubGroups; group++)
+      {
+        // printf("Offset: %3u\n", offset);
+        unsigned int startIndex = resultGroups[group].rm_so;
+        unsigned int endIndex = resultGroups[group].rm_eo;
+        if (resultGroups[group].rm_so == -1)
+        {
+          break; // 当該部分内の、グループが存在しなかったら終了
+        }
+        else
+        {
+          char matchedText[STR_LENGTH] = "";
+          strncpy(matchedText, &cursor[startIndex],
+                  endIndex - startIndex);
+          if (group == 0)
+          {
+            // 当該部分全体を表すグループの場合
+            printf("%2u 個目の当該部分: [%2u -> %2u] %s\n", match + 1, startIndex, endIndex, matchedText);
+            strcpy(dest[match], matchedText);
+            // offset = endIndex;
+            nextCursor = cursor + endIndex; // 正規表現を実行する範囲をこの当該部分以降にする
+          }
+          else
+          {
+            // 当該部分内の一部分を指すグループの場合
+            printf("| %2u 個目のグループ: [%2u -> %2u] %s\n", group, startIndex, endIndex, matchedText);
+          }
+        }
       }
     }
+    cursor = nextCursor;
   }
+
   regfree(&preg);
   return matchCount;
 }
 
-
-
-
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
   char *fileName = argv[1];
   char text[STR_LENGTH] = "";
   char phoneNumbers[PHONENUMBER_COUNT][16];
-  if(argc < 2){
+  if (argc < 2)
+  {
     printf("対象のファイル名を引数に明記してください。\n");
     return 1;
   }
   printf("ファイル %s から読み込み開始: \n", fileName);
-  if(readTextFile(text, fileName) == ERROR_FILEHANDLE){
+  if (readTextFile(text, fileName) == ERROR_FILEHANDLE)
+  {
     printf("読み込みに失敗しました。\n");
     return 1;
   }
-  printf("抽出対象テキスト: ==========\n%s\n=================================\n", text);
+  printf(
+      "抽出対象テキスト: ==========\n%s\n=================================\n",
+      text);
   printf("結果: \n");
-  // char *regex = "0[7,8,9]0-[0-9]{3,4}-[0-9]{4,5}";
-  char *regex = "代表";
-  int count = findMatches(regex, text, phoneNumbers, PHONENUMBER_COUNT);
-  if(count == ERROR_REGEX){
+  // char *regex = "(0[7,8,9]0)-([0-9]{3,4})-([0-9]{4,5})";
+  // const char regex[] = "0[7,8,9]0-\\d{3,4}-\\d{4,5}";
+  char regex[] = "(0[7,8,9]0)-([[:digit:]]{3,4})-([[:digit:]]{4,5})";
+  // const char regex[] = "0[7,8,9]0-[0-9]{3,4}-[0-9]{4,5}";
+  // char *regex = "代表";
+  int count = findMatches(regex, text, phoneNumbers);
+  if (count == ERROR_REGEX)
+  {
     printf("正規表現は利用不可能です。\n");
     return 1;
   }
-  for(int i = 0; i < count; i++){
+  for (int i = 0; i < count; i++)
+  {
     printf("[%d件目] %s\n", i + 1, phoneNumbers[i]);
   }
   return 0;
